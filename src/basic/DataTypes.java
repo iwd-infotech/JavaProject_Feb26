0f79219e73eac22c88c29d071c694324b5713a0e
selenium28 <devselenium.class2819@gmail.com>
2026-02-17 20:49:12 +0530

    Basic program

---
+++ src/basic/DataTypes.java
@@ -0,0 +1,49 @@
+package basic;
+
+public class DataTypes {// class body
+
+	// main method
+	public static void main(String[] args) {
+//		DataTypes
+//		1. Int 
+		int a;
+		a = 10;
+		System.out.println(a);
+		
+		// int - datatype, b = variable, 20 is a value.
+		int n = 20;
+		System.out.println(n);
+//		int m = 33.33;  Type mismatch: cannot convert from double to int
+		
+//		2.Double
+		double d = 33.33;
+		double d1 = 100;   // 100, 100.00, 100.0
+		System.out.println(d);
+		System.out.println(d1);
+		
+//		3. char
+		char c = 'P';
+		char c1 = '#';
+		System.out.println(c1);
+		
+//		4. Boolean
+//		What are true & false in java?
+//		true & false are the keywords in java also we can use as a value.
+		boolean b = true;
+		boolean b1 = false;
+		
+		System.out.println(b);
+		
+//		5. String 
+//		String is a class in java
+//		String is a sequence of char.
+//		In java every class name starting from capital case
+		String s = "Seema";
+		System.out.println(s);
+		String s1 = "12345";
+		System.out.println(s1);
+	
+	}
+
+}
+
